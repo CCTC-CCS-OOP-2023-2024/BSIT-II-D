4733432bbf7ac9cdfc57d571cccfc2cd2d4d1acf
ArnelDemotor <134209373+ArnelDemotor@users.noreply.github.com>
2023-12-15 11:06:02 +0800

    Add files via upload

---
+++ Projects/BrewBitesdemotor by Arnel Demotor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class ARNELBREWBITES
+{
+    static void Main()
+    {
+        // Module 1 - Getting started
+        Console.WriteLine("***************************");
+        Console.WriteLine("** Welcome to BREWBITES! **");
+        Console.WriteLine("***************************");
+
+
+        // Basic Concepts (Output)
+        Console.WriteLine("-Enjoy our cozy atmosphere.-");
+
+        // Variables
+        int numberOfTables = 10;
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"|*We have {numberOfTables} tables.            |");
+        
+        // Data Types
+        double coffeePrice = 2.5;
+        Console.WriteLine($"|*A cup of coffee costs ${coffeePrice}.   |");
+
+
+        // Doing Math
+        int sugarPacketsPerCoffee = 2;
+        double totalSugarUsed = numberOfTables * sugarPacketsPerCoffee;
+        Console.WriteLine($"|*We use {totalSugarUsed} sugar packets daily.|");
+        Console.WriteLine("---------------------------------");
+    }
+}
