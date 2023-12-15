a2af757230e5dfef0c8de901153cd531ed4dd7ac
Jill Cris G. Juntong <152982875+jillcris172@users.noreply.github.com>
2023-12-15 09:46:18 +0800

    Update Recipenote Juntong Jill Cis.cs

--- Projects/Recipenote Juntong Jill Cis.cs
+++ Projects/Recipenote Juntong Jill Cis.cs
@@ -1,6 +1,1 @@
-﻿Build started...
-1>------ Build started: Project: Note.cs, Configuration: Debug Any CPU ------
-1>Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
-1>Note.cs -> C:\Users\PC\source\repos\Note.cs\Note.cs\bin\Debug\net6.0\Note.cs.dll
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build started at 9:26 AM and took 01.507 seconds ==========
+
